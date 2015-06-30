2102f5fd459592c8aae4e694730a9965b7a70522
rlvrs <rosantos@student.dei.uc.pt>
2015-06-30 18:31:01 +0100

    testing files

--- a.c
+++ a.c
@@ -1,17 +1,24 @@
-#include<stdio.h>
-#include"a.h"
+typedef struct {
+	int a;
+}time_t;
 
-void sys_1(struct st st1)
-{
-	printf("%d, %d\n", st1.a, st1.b);
-}
+typedef struct {
+	int b;
+}size_t;
 
-int main ()
-{
-	struct st st1 = 
-	{
-		.a = 2,
-		.b = 3
-	};
-	sys_1(st1);
-}
+typedef struct {
+	int a;
+}uid_t;
+
+typedef struct {
+	int a;
+}gid_t;
+
+typedef struct {
+	int a;
+}pid_t;
+
+char* sys_1(struct st *st1, const struct st* st2, unsigned a, time_t *tloc, long c, unsigned long d) {}
+char* sys_2(void) {}
+char* sys_3(int asd) {}
+char* sys_4(time_t *tloc) {}
--- a.h
+++ a.h
@@ -3,3 +3,30 @@ struct st
 	int a;
 	int b;
 };
+struct _st2
+{
+	int z;
+	int y;
+};
+struct st
+{
+	int a;
+	int b;
+};
+typedef struct {
+	int a;
+}time_t;
+
+typedef struct {
+	int b;
+}size_t;
+
+typedef struct {
+	int c;
+}uid_t;
+
+typedef struct {
+	int d;
+}gid_t;
+typedef int		__kernel_pid_t;
+typedef __kernel_pid_t          pid_t;
